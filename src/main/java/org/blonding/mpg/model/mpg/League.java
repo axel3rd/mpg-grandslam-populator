344cb719a701009eca7d492bc67441edb00b5c56
Alix Lourme <alix.lourme@gmail.com>
2020-12-13 01:29:40 +0100

    Fix CLI exec / JAR size / README / SpringBatch metadata into DB remove

--- src/main/java/org/blonding/mpg/model/mpg/League.java
+++ src/main/java/org/blonding/mpg/model/mpg/League.java
@@ -1,9 +1,13 @@
 package org.blonding.mpg.model.mpg;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class League {
+public class League implements Serializable {
+
+    private static final long serialVersionUID = 4749106603974234336L;
 
     private String id;
     private String name;
--- src/main/java/org/blonding/mpg/model/mpg/Team.java
+++ src/main/java/org/blonding/mpg/model/mpg/Team.java
@@ -1,10 +1,14 @@
 package org.blonding.mpg.model.mpg;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Team {
+public class Team implements Serializable {
+
+    private static final long serialVersionUID = -837193093869797806L;
 
     private String userId;
 
